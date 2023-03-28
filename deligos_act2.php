f9045133b8b3faf0313a68d69939fd4cbdd6c7b0
jessseduard <deligosjess@gmail.com>
2023-03-28 16:23:40 +0800

    multiplication table

---
+++ deligos_act2.php
@@ -0,0 +1,16 @@
+<hmtl>
+<head><title>MULTIPLICATION</title>
+</head>
+<body>
+    <center>
+    <h4>Multiplication Table of number 8</h4>
+    <?php
+        $number = 8;
+
+    for($i=1; $i<=10; $i++){
+            echo $number ."x". $i ."=". $number * $i ."<br>";
+        }
+    ?>
+    </center>
+</body>
+</html>
